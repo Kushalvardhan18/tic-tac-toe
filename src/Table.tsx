8881d38f8d4764157f0e1a654c38b0eab170bedc
Kushal Vardhan <vardhank140102@gmail.com>
2026-05-19 12:01:00 +0530

    reset button and functionality added

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import circle from "./assets/circle.png";
 import cross from "./assets/cross.png";
-
+import reset from "./assets/reset.png";
 function Table() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [isCross, setIsCross] = useState(true);
@@ -14,28 +14,39 @@ function Table() {
     setBoard(newBoard);
     setIsCross(!isCross);
   }
+  function resetGame() {
+    setBoard(Array(9).fill(null));
+    setIsCross(true);
+  }
 
   return (
-  <div className="text-white flex justify-center items-center mt-30">
-  <div className="grid grid-cols-3 gap-2 p-4 bg-gray-800 rounded-xl shadow-2xl">
-    {board.map((item, index) => (
+    <div className="text-white flex flex-col gap-10 justify-center items-center mt-10">
       <div
-        key={index}
-        onClick={() => showImage(index)}
-        className="w-30 h-30 border border-gray-600 bg-gray-900 flex items-center justify-center cursor-pointer 
-                   shadow-md hover:border-gray-800 transition-all duration-200"
+        className="flex flex-col justify-center items-center cursor-pointer hover:text-red-600 "
+        onClick={resetGame}
       >
-        {item && (
-          <img
-            src={item}
-            alt={item === cross ? "cross" : "circle"}
-            className="w-20 h-20"
-          />
-        )}
+        <img src={reset} alt="reset" />
+        <div className="text-xl">Reset</div>
       </div>
-    ))}
-  </div>
-</div>
+      <div className="grid grid-cols-3 gap-2 p-4 bg-gray-800 rounded-xl shadow-2xl">
+        {board.map((item, index) => (
+          <div
+            key={index}
+            onClick={() => showImage(index)}
+            className="w-30 h-30 border border-gray-600 bg-gray-900 flex items-center justify-center cursor-pointer 
+                   shadow-md hover:border-gray-800 transition-all duration-200"
+          >
+            {item && (
+              <img
+                src={item}
+                alt={item === cross ? "cross" : "circle"}
+                className="w-20 h-20"
+              />
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 export default Table;
