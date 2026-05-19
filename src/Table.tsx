ff5ac5748de31722150895cb2390a1d6e3c175f8
Kushal Vardhan <vardhank140102@gmail.com>
2026-05-19 12:58:48 +0530

    winner detction functionality added

--- src/Table.tsx
+++ src/Table.tsx
@@ -3,24 +3,48 @@ import circle from "./assets/circle.png";
 import cross from "./assets/cross.png";
 import reset from "./assets/reset.png";
 function Table() {
-  const [board, setBoard] = useState(Array(9).fill(null));
+  const [board, setBoard] = useState<(string | null)[]>(Array(9).fill(null));
   const [isCross, setIsCross] = useState(true);
+
+  const [winner, setWinner] = useState("");
 
   function showImage(index: number) {
     if (board[index] !== null) return;
 
     const newBoard = [...board];
-    newBoard[index] = isCross ? cross : circle;
+    newBoard[index] = isCross ? "X" : "0";
     setBoard(newBoard);
     setIsCross(!isCross);
+    checkWinner(newBoard);
+  }
+
+  function checkWinner(board: (string | null)[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (const [a, b, c] of lines) {
+      if (board[a] && board[a] === board[b] && board[b] == board[c]) {
+        setWinner(board[a] === "X" ? "Cross" : "Circle" );
+      }
+    }
   }
   function resetGame() {
     setBoard(Array(9).fill(null));
     setIsCross(true);
+    setWinner("");
   }
 
   return (
     <div className="text-white flex flex-col gap-10 justify-center items-center mt-10">
+      <h2>{winner || "draw"}</h2>
       <div
         className="flex flex-col justify-center items-center cursor-pointer hover:text-red-600 "
         onClick={resetGame}
@@ -38,8 +62,8 @@ function Table() {
           >
             {item && (
               <img
-                src={item}
-                alt={item === cross ? "cross" : "circle"}
+                src={item === "X" ? cross : circle}
+                alt={item === "X" ? "cross" : "circle"}
                 className="w-20 h-20"
               />
             )}
